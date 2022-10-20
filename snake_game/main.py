ae6c9440b7ae92ca5c7f0cf5174942660fe386c6
Amna Rahman <amnarahman00@gmail.com>
2022-10-20 19:16:03 +0500

    Improved Snake Game

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,54 +1,60 @@
-
-import turtle
+from turtle import Turtle , Screen
 import time
-from scoreboard import Scoreboard
-from snake import Snake
+from classes import Snake 
 from food import Food
+from scoreboard import Scoreboard
 
-new_square = turtle.Turtle()
-screen  = turtle.Screen()
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.setup(600,600)
+#Setting up Screen
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor('black')
+screen.title('Snake Game')
 screen.tracer(0)
 
+#Holding snake with keys
 
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-screen.listen()
-screen.onkey(snake.up, "Up") 
-screen.onkey(snake.down, "Down") 
-screen.onkey(snake.left, "Left") 
-screen.onkey(snake.right, "Right") 
+#Controlling  the snake with keys
 
+screen.listen()
+screen.onkey(snake.up , "Up")
+screen.onkey(snake.down ,"Down")
+screen.onkey(snake.left , "Left")
+screen.onkey(snake.right , "Right")
+
+# Moving the snake 
 
 game_is_on = True
-
 while game_is_on:
-     screen.update()
-     time.sleep(0.1)
-     snake.move()
-     
-      #Detect collision wih food
-     if snake.head.distance(food) < 15:
-          food.refresh()
-          snake.extend()
-          scoreboard.increase_score()
-          
-          
-     #Detect collision with wall.
-     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+# Collision detection with food
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+#COllision with Wall
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        scoreboard.reset()
+        snake.reset()
         
-   #Detect collision with tail.
-     for segment in snake.segments[1: ]:
-          if segment == snake.head:
-            pass
-          elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+
+
+
+# Collision with Tail
+    for segment in snake.segments[1:]:  #Slicing
+        if snake.head.distance(segment)<10:
+            scoreboard.reset()
+            snake.reset()
+
+
+
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,31 +1,36 @@
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
 
+ALIGNMENT = "center"
+FONT = ("Courier" , 18,"normal")
 
 class Scoreboard(Turtle):
-    
-    def __init__(self) :
-        
+    def __init__(self):
         super().__init__()
-        self.score = 0
-        self.color("white")
+        self.score=0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
+            
+        self.color('white')
         self.penup()
         self.goto(0,270)
         self.hideturtle()
-        self.update_scoreboard()
+        self.write(f"Score: {self.score} High Score: {self.high_score}" , align = ALIGNMENT , font = FONT)
         
-    
-    def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)    
-    
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-    
-    
-    def increase_score(self):
-        self.score += 1
+    def update_score(self):
         self.clear()
-        self.update_scoreboard()
-        
+        self.write(f"Score: {self.score} High Score: {self.high_score}" , align = ALIGNMENT , font = FONT)
+
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt" , mode ="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+
+    def increase_score(self):
+        self.score+=1
+        self.write(f"Score: {self.score}" , align = ALIGNMENT , font = FONT)
+        self.update_score()
